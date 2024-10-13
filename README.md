479725db51e460c4553d57b68d18ab2b06785d98
Adm-2005 <akshatdmishra2005@gmail.com>
2024-10-13 15:35:30 +0530

    added support for sklearn 1.5.2

---
+++ README.md
@@ -0,0 +1,177 @@
+<center>
+<h1>CarSearchKaro</h1>
+</center>
+<br>
+
+## 📌 Useful links:
+
+<br>
+
+<h2> Document file Link : 
+<a href="https://drive.google.com/file/d/1pgOjvcFya-2zpKJ2SreOqtu0X1p0Ez5u/view?usp=sharing">HAVE A LOOK</a>
+</h2> 
+
+<h2> Youtube video demo Link : 
+<a href="https://youtu.be/9TP7GvM8vyI">HAVE A LOOK</a>
+</h2> 
+
+<h2> Python jupyter notebook Link : EDA and Prediction on Automobile Dataset
+<a href="https://colab.research.google.com/drive/1tezf6JpbFDKLVrC2cREVnK5W55L4kDKb?usp=sharing">HAVE A LOOK</a>
+</h2> 
+<br>
+
+## 📌 How to run this projects:
+
+<ul>
+<li>
+<h3>Clone the project</h3>
+</li>
+<li>
+<h3>Install requirements.txt</h3>
+
+pip install -r requirements.txt
+</li>
+
+<li>
+<h3>Run Project on terminal </h3>
+
+python manage.py runserver
+
+Starting development server at http://127.0.0.1:8000/
+
+will appear on the terminal click on the http 
+</li>
+<li>
+<h3>The website will run on the localhost</h3>
+</li>
+</ul>
+<br><br>
+
+## 📌 Tech Stack:
+<br>
+
+[![HTML](https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white)]
+[![CSS](https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white)]
+[![JS](https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)]
+[![PYTHON](https://img.shields.io/badge/python%20-%23E34F26.svg?&style=for-the-badge&logo=python&logoColor=white)]
+[![DJANGO](https://img.shields.io/badge/django%20-%23323330.svg?&style=for-the-badge&logo=Django&logoColor=%23F7DF1E)]
+
+<br><br>
+
+## 📌 About the project:
+<br>
+<ul>
+<li>CarSearchKaro is a data analysis and machine learning venture that helps users search cars that meet their requirements.</li>
+<li>It carries automotive content such as pleasing data visuals, modern data analysis and AI chatbot for cars of various brands available in the market.</li>
+<li>The web application includes many innovative features to ensure that users get a detailed price estimation based on the filters added such as brand, mileage, price range and a hist of other factors and before visiting a dealer showroom. It also permits the user to provide inputs that they feel might help other users.</li>
+<li>Our vision is to construct a complete ecosystem for prospective buyers so that they have easy and complete access to information and data that will help them in making the right car choice. </li>
+</ul>
+<br><br>
+
+## 📌 Website flow:
+<br>
+<img src="static\readmeimage\websiteflow.png" alt="header" width="600px" height="500px">
+
+<br><br>
+
+## 📌 Main Page:
+<br><br>
+
+<h2>HOME SECTION</h2>
+<br>
+<img src="selectCar\static\readmeimage\home.png" alt="header" width="700px" height="400px">
+<br><br>
+<h2>BEST SECTION</h2>
+<br>
+<img src="selectCar\static\readmeimage\bestcar.png" width="700px" height="400px">
+<img src="selectCar\static\readmeimage\bestcar1.png" width="700px" height="400px">
+
+<br><br>
+<h2>ANALYSIS SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\analysis.png" width="700px" height="400px">
+<br><br>
+<h2>DASHBOARD SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\dashboard.png" width="700px" height="400px">
+<br><br>
+<img src="selectCar\static\readmeimage\dashboard1.png" width="700px" height="400px">
+<br><br>
+<img src="selectCar\static\readmeimage\dashboard2.png" width="700px" height="400px">
+<br><br>
+<h2>SERVICES SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\service.png" alt="animation" width="700px" height="400px">
+<br><br>
+<h2>CHATBOT SECTION</h2>
+<br>
+<img src="selectCar\static\readmeimage\chatbot.png" width="700px" height="400px">
+<br><br>
+<img src="selectCar\static\readmeimage\chatbot1.png" width="700px" height="400px">
+<br><br>
+<img src="selectCar\static\readmeimage\chatbot2.png" width="700px" height="400px">
+
+<br><br>
+<h2>PRICE PREDICTION SECTION</h2>
+<br>
+<img src="static\readmeimage\ml.png" width="700px" height="400px">
+<br><br>
+<img src="static\readmeimage\ml1.png" width="700px" height="400px">
+
+<br><br>
+
+<h2>FEATURED SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\featured.png" width="700px" height="400px">
+<br><br>
+<img src="selectCar\static\readmeimage\featured1.png" width="700px" height="400px">
+<br><br>
+<h2>TOP BRANDS SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\topbrand.png" width="700px" height="400px">
+<br><br>
+
+<h2>CONTACT SECTION</h2>
+<br>
+<img src="selectCar\static\readmeimage\contact.png" width="700px" height="400px">
+<br><br>
+
+<h2>DATABASE FOR CONTACT SUPPORT SECTION</h2>
+<br>
+<h4>Type Admin at the top url section with previous url</h4>
+<h4>example:http://127.0.0.1:8000/admin</h4>
+<br>
+This is how datbase team can check the stored customer information
+<br>
+<img src="static\readmeimage\djangologin.png" width="700px" height="400px">
+<br><br>
+<img src="static\readmeimage\panel.png" width="700px" height="400px">
+<br><br>
+<img src="static\readmeimage\panel1.png" width="700px" height="400px">
+
+
+<br><br>
+<h2>FOOTER SECTION</h2>
+<br>
+
+<img src="selectCar\static\readmeimage\footer.png" width="700px" height="400px">
+<br><br>
+
+<h2>📌 Contact :</h2>
+<br><br>
+
+<div align="center">
+
+<a  href="https://www.linkedin.com/in/shruti-mishra-b270a7203/" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin%20-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a><span>
+<a href="mailto:shrutidmishra2002@gmail.com"><img  alt="Gmail" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"/></span>
+
+</div>
+<br>
+<div align="center">
+© 2021 Shruti Mishra </div>
+
---
+++ selectCar/views.py
@@ -0,0 +1,257 @@
+# from ast import Str
+# from asyncio.windows_events import NULL
+# from copyreg import pickle
+from django.shortcuts import render
+from django.views import View
+import pandas as pd
+# import plotly.express as px
+# import json
+from .models import ContactUs
+import pickle
+import numpy as np
+
+# Read data from csv file
+df = pd.read_csv("selectCar/data/processed_cardekho.csv")
+model = pickle.load(open('selectCar/data/rfregressor.pkl', "rb"))
+
+# Current car details
+car: dict = {
+            'brand': "",
+            'year': "",
+            'price': "",
+            'cars': "",
+            'msg': "",
+            'dataSet': "",
+            'alert': ""
+            }
+
+# Class Based rendering the index page
+class Home(View):
+    
+    def get(self, request):
+        car['alert']=""
+        return render(request, 'selectCar/index.html')
+
+    def post(self, request):
+        if request.method == 'POST':
+            name = request.POST['name']
+            if name.find(' ') != -1:
+                first_name, Last_name = name.split(' ', 1)
+            else:
+                first_name = name
+                Last_name = name
+            email = request.POST['email']
+            subject = request.POST['subject']
+            message = request.POST['message']
+            objects = ContactUs()
+            data = ContactUs.objects.create(first_name=first_name, last_name=Last_name, User_email=email, User_subject=subject, User_message=message)
+            data.save()
+            car['alert'] = "Thankyou for contacting us! Our team will get back to you soon."
+        else:
+            car['alert'] = "Please fill the form again."
+        return render(request, 'selectCar/index.html',{'car':car})
+     
+# Class Based rendering the Car page
+class Car(View):
+    
+    # Get Function to render the car page
+    def get(self, request):
+        tempDF = df.copy() 
+        tempOptions = tempDF["Brand"].unique()
+        car['cars'] = tempOptions
+        car['msg'] = ""  
+        tempDF = tempDF.head()
+        car['dataSet'] = len(tempDF)
+        return render(request, 'selectCar/car.html',{"car":car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+    
+    # Post Function to render the car page
+    def post(self, request):       
+        tempDF = df.copy()
+        tempOptions = tempDF["Brand"].unique()
+        car['cars'] = tempOptions
+        car['msg'] = ""
+        
+        # If the Brand Year Price is not NULL then assign value to car & Type Cast String to Int
+        if request.POST["Brand"]:
+            car['brand'] = request.POST["Brand"]
+        if request.POST["price"]:
+            int_price = int(request.POST["price"])
+            car['price'] = int_price
+        if request.POST["year"] :
+            int_year = int(request.POST["year"])
+            car['year'] = int_year
+                    
+        # Filter the dataframe based Brand Year Price
+        if request.POST["Brand"] and request.POST["year"] and len(request.POST["year"])==4 and request.POST["price"] and len(request.POST["price"])>4:
+            tempDF = tempDF[(tempDF["Brand"]==request.POST["Brand"]) & (tempDF["year"]==int_year) & (tempDF["selling_price"]<int_price)].sort_values('selling_price', ascending=False)
+            print(tempDF,"Brand Year Price..................")
+        
+        # Filter the dataframe based Brand Year
+        elif request.POST["Brand"] and request.POST["year"] and len(request.POST["year"])==4:
+            tempDF = tempDF[(tempDF["Brand"]==request.POST["Brand"]) & (tempDF["year"]==int_year)].sort_values('selling_price', ascending=False)
+            print(tempDF,"Brand Year....................")
+        
+        # Filter the dataframe based Brand Price
+        elif request.POST["Brand"] and request.POST["price"] and len(request.POST["price"])==4:
+            tempDF = tempDF[(tempDF["Brand"]==request.POST["Brand"]) & (tempDF["selling_price"]<int_price)].sort_values('selling_price', ascending=False)
+            print(tempDF,"Brand Price....................")
+        
+        # Filter the dataframe based Year Price
+        elif request.POST["year"]  and len(request.POST["year"])==4 and request.POST["price"] and len(request.POST["price"])>4:
+            tempDF = tempDF[(tempDF["year"]==int_year) & (tempDF["selling_price"]<int_price)].sort_values('selling_price', ascending=False)
+            print(tempDF,"Year Price....................")
+        
+        # Filter the dataframe based on Brand only
+        elif request.POST["Brand"]:
+            tempDF = tempDF[tempDF["Brand"]==request.POST["Brand"]].sort_values('selling_price', ascending=False)
+            # print(tempDF,"Brand ........")
+            
+        # Filter the dataframe based on Year only
+        elif request.POST["year"] and len(request.POST["year"])==4:
+            tempDF = tempDF[tempDF["year"]==int_year].sort_values('selling_price', ascending=False)
+            # print(tempDF,"Year ........")
+        
+        # filter the dataframe based on Price only  
+        elif request.POST["price"] and len(request.POST["price"])>4:
+            tempDF = tempDF[tempDF["selling_price"]<int_price].sort_values('mileagev', ascending=False)
+            # print(tempDF,"Price ........") 
+                   
+        # Wrong input or invalid input or over the limit input then show the error message
+        else :
+            car['msg'] = "Please Enter Valid Year and Amount, Like 2018 and Amount between 1,00,000 to 10,00,000."
+            tempDF = tempDF.head()
+            # print("Wrong Input ........")
+        
+        # Check the dataframe is empty or not if empty then show the message
+        if tempDF.empty:
+            car['msg'] = "No Car Found with the given details"
+        
+        # Collect the dataframe data and sort the head and send to render the car page
+        tempDF = tempDF.head()
+        car['dataSet'] = len(tempDF)
+        return render(request, 'selectCar/car.html',{"car":car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+  
+  
+# Prediction
+class Prediction(View):
+    
+    def get(self, request):
+        return render(request, 'selectCar/prediction.html')
+    
+    def post(self, request):
+        # if request.POST["selling_price"]:
+        #     selling_price = int(request.POST["selling_price"])
+        #     print(selling_price)
+        
+        if request.POST["km_driven"]:
+            km_driven = int(request.POST["km_driven"])
+            print(km_driven)
+            
+        if request.POST["transmission"]:
+            transmission = int(request.POST["transmission"])
+            print(transmission)
+            
+        if request.POST["owner"]:
+            owner = int(request.POST["owner"])
+            print(owner)
+            
+        if request.POST["mileage"]:
+            mileage = float(request.POST["mileage"])
+            print(mileage)
+            
+        if request.POST["engine"]:
+            engine = int(request.POST["engine"])
+            print(engine)
+            
+        if request.POST["max_power"]:
+            max_power = int(request.POST["max_power"])
+            print(max_power)
+            
+        if request.POST["brand"]:
+            brand = int(request.POST["brand"])
+            print(brand)
+	    
+        UserInput = []
+        UserInput.append(km_driven)
+        UserInput.append(transmission)
+        UserInput.append(owner)
+        UserInput.append(mileage)
+        UserInput.append(engine)
+        UserInput.append(max_power)
+        UserInput.append(brand)
+        l=np.array([UserInput])
+        l.reshape(-1, 1)
+
+        result = model.predict(l)
+
+        return render(request, 'selectCar/prediction.html',{'result':result})
+  
+
+    # """ --->> Function basded rendering car page with dynamic data & plot Chart.JS Graph and Datatable <<--- """ #
+
+# 1. Which is the Best Mileage Car  
+def data1(request):
+    tempDF = df.copy()
+    tempDF = tempDF.sort_values('mileagev', ascending=False)
+    tempDF = tempDF.head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+# 2. Car's showcased according to the Selling Price Range (Min)
+def data2(request):
+    tempDF = df.copy()
+    tempDF = tempDF[(tempDF["selling_price"]>=400000)].sort_values('mileagev', ascending=False)
+    tempDF = tempDF.head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+# 3. Car's showcased according to the Selling Price Range (Max)
+def data3(request):
+    tempDF = df.copy()
+    tempDF = tempDF[(tempDF["selling_price"]<=800000)].sort_values('mileagev', ascending=False)
+    tempDF = tempDF.head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+
+# 4. The Highest Selling Price cars?
+def data4(request):
+    tempDF = df.copy()
+    tempDF = tempDF.sort_values('selling_price', ascending=False).head()
+    tempDF = tempDF.head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+
+# 5. Which are the High Power and Low Selling Price car models?
+def data5(request):
+    tempDF = df.copy()
+    tempDF = tempDF.sort_values('selling_price', ascending=True).head()
+    tempDF = df.sort_values('max_powerv', ascending=False).head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+# 6. Which are the Max Mileage and Max Power car models?
+def data6(request):
+    tempDF = df.copy()
+    tempDF = df.sort_values('mileagev', ascending=False).head()
+    tempDF=  df.sort_values('max_powerv', ascending=False).head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+
+# 7. Which are the Max Engine and Max Power car models?
+def data7(request):
+    tempDF = df.copy()
+    tempDF = tempDF.sort_values('enginev', ascending=False).head()
+    tempDF=  tempDF.sort_values('max_powerv', ascending=False).head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
+
+# 8. Which are the Max Mileage and Max Engine car models?
+def data8(request):
+    tempDF = df.copy()
+    tempDF = tempDF.sort_values('mileagev', ascending=False).head()
+    tempDF=  tempDF.sort_values('enginev', ascending=False).head()
+    car['dataSet'] = len(tempDF)
+    return render(request, 'selectCar/car.html',{'car':car, "dataT":tempDF.values.tolist(), "dataY":str(tempDF["selling_price"].values.tolist()), "dataX": str(tempDF["name"].values.tolist())})
